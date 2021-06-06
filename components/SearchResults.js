1920d4c9045e3f277cd75e8b24b02108cbab6576
Mahdi Rezayi <mahdi.rezzai93@gmail.com>
2021-06-06 21:06:35 +0430

    First Deployment

---
+++ components/SearchResults.js
@@ -0,0 +1,17 @@
+import Post from '@/components/Post'
+
+export default function SearchResults({ results }) {
+  if(results.length === 0) return <></>
+
+  return (
+    <div className='absolute top-20 right-0 md:right-10 z-10 border-4 border-gray-500 bg-white text-black w-full md:w-6/12 rounded-2xl'>
+      <div className='p-8'>
+        <h2 className='text-2xl mb-3'>{results.length} Results</h2>
+
+        {results.map((result, index) => (
+          <Post key={index} post={result} compact />
+        ))}
+      </div>
+    </div>
+  )
+}
